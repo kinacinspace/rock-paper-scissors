b477d68a1a379a770f53ee1597f602bb18922f9d
kinacinspace <34605373+kinacinspace@users.noreply.github.com>
2023-04-02 00:07:28 +0300

    Add computer picking function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello world!!!")
+const ROCK = 0
+const PAPER = 1
+const SCISSORS = 2
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }  
+
+function getComputerChoice(){
+    switch (getRandomInt(1, 4)){
+        case 1: return ROCK;
+        case 2: return PAPER;
+        case 3: return SCISSORS;
+    }
+}
+
